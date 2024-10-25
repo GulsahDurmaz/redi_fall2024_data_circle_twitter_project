0d14b1202cbad54f70ba9c7c3a1bce4bcbfea3a8
Gülsah Durmaz <zahidegulsahdurmaz@gmail.com>
2024-10-25 10:50:17 +0200

    added csvs

--- sentimental_data_analysis.py
+++ sentimental_data_analysis.py
@@ -1,52 +1,52 @@
-# sentimental_data_analysis.py
-# from imports import *
+# # sentimental_data_analysis.py
+# # from imports import *
 
-def run_sentimental_data_analysis(trump_df, biden_df):
-    # Load the dataset
-    language_dataset = pd.read_csv('/Users/gulsah/Desktop/REDI/redi_fall2024_data_circle_twitter_project/csv/Language Detection.csv')
+# def run_sentimental_data_analysis(trump_df, biden_df):
+#     # Load the dataset
+#     language_dataset = pd.read_csv('/Users/gulsah/Desktop/REDI/redi_fall2024_data_circle_twitter_project/csv/Language Detection.csv')
 
-    # Data cleaning
-    # 1. Lowercase conversion
-    language_dataset['Text'] = language_dataset['Text'].str.lower()
-    # 2. Remove emojis
-    language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'[^\w\s,]', '', x))
-    # 3. Remove URLs
-    language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'http\S+|www\S+', '', x))
-    # 4. Remove mentions & hashtags
-    language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'@\w+|#\w+', '', x))
-    # 5. Remove punctuation, numbers, and special characters
-    language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
+#     # Data cleaning
+#     # 1. Lowercase conversion
+#     language_dataset['Text'] = language_dataset['Text'].str.lower()
+#     # 2. Remove emojis
+#     language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'[^\w\s,]', '', x))
+#     # 3. Remove URLs
+#     language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'http\S+|www\S+', '', x))
+#     # 4. Remove mentions & hashtags
+#     language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'@\w+|#\w+', '', x))
+#     # 5. Remove punctuation, numbers, and special characters
+#     language_dataset['Text'] = language_dataset['Text'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
 
-    # Feature extraction: convert text into numerical features (Bag of Words)
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(language_dataset['Text'])  # Text data
-    y = language_dataset['Language']  # Language labels
+#     # Feature extraction: convert text into numerical features (Bag of Words)
+#     vectorizer = CountVectorizer()
+#     X = vectorizer.fit_transform(language_dataset['Text'])  # Text data
+#     y = language_dataset['Language']  # Language labels
 
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#     # Split data into training and testing sets
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Model selection: Multinomial Naive Bayes
-    model = MultinomialNB()
-    model.fit(X_train, y_train)
+#     # Model selection: Multinomial Naive Bayes
+#     model = MultinomialNB()
+#     model.fit(X_train, y_train)
 
-    # Make predictions
-    y_pred = model.predict(X_test)
+#     # Make predictions
+#     y_pred = model.predict(X_test)
 
-    # Evaluate the model
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f'Accuracy: {accuracy * 100:.2f}%')
+#     # Evaluate the model
+#     accuracy = accuracy_score(y_test, y_pred)
+#     print(f'Accuracy: {accuracy * 100:.2f}%')
 
-    # Feature extraction: convert text into numerical features (Bag of Words)
-    text_transformed = vectorizer.transform(trump_df['tweet'])
-    trump_df['predicted_language'] = model.predict(text_transformed)
-    # Display the DataFrame with the predictions
-    # trump_df[['tweet', 'predicted_language']]
-    st.dataframe(trump_df.groupby('predicted_language')['tweet'].count())
+#     # Feature extraction: convert text into numerical features (Bag of Words)
+#     text_transformed = vectorizer.transform(trump_df['tweet'])
+#     trump_df['predicted_language'] = model.predict(text_transformed)
+#     # Display the DataFrame with the predictions
+#     # trump_df[['tweet', 'predicted_language']]
+#     st.dataframe(trump_df.groupby('predicted_language')['tweet'].count())
 
-    # Create a sample TextBlob
-    english_tweets = trump_df[trump_df['predicted_language'] == 'English']
-    # Get sentiment
-    english_tweets['sentiment'] = english_tweets['tweet'].apply(lambda tweet: TextBlob(tweet).sentiment.polarity)
+#     # Create a sample TextBlob
+#     english_tweets = trump_df[trump_df['predicted_language'] == 'English']
+#     # Get sentiment
+#     english_tweets['sentiment'] = english_tweets['tweet'].apply(lambda tweet: TextBlob(tweet).sentiment.polarity)
 
-    # Check the results
-    st.dataframe(english_tweets[['tweet', 'sentiment']].head(10))
+#     # Check the results
+#     st.dataframe(english_tweets[['tweet', 'sentiment']].head(10))
